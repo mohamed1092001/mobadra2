9650de67db129359509f54b360348f9b15f0dc9a
mohamed hossam <101831478+mohamed1092001@users.noreply.github.com>
2024-09-19 00:24:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
 
 
         $(".screen-dimensions").css({ "width": fittedWidth + 'px', "height": fittedHeight + 'px' })
+        $(".num").css({ "width": (fittedWidth * (46.63 / 1080)) + 'px ',"padding":"0px"})
 
         // حساب حجم الخط بناءً على أصغر بُعد
         var minDimension = Math.min(fittedWidth, fittedHeight);
@@ -29,13 +30,14 @@ $(document).ready(function () {
         $(".hero p").css("font-size", (fontSize * .036635) + 'px');
         $(".hero button").css("font-size", (fontSize * .036) + 'px');
         $(".reyada-ar p").css("font-size", (fontSize * .0326) + 'px');
-        $(".wakeel-ar p").css("font-size", (fontSize * .0326) + 'px');
+        $(".wakeel-ar p").css("font-size", (fontSize * .035) + 'px');
         $(".nashr-ar p").css("font-size", (fontSize * .032) + 'px');
         $(".ar-section .mid-title").css("font-size", (fontSize * .057) + 'px');
         $(".ar-section .goal-box p").css("font-size", (fontSize * .027) + 'px');
         $(".ar-section .center-goal-box p").css("font-size", (fontSize * .023) + 'px');
         $(".ar-section .goal-row .mid-title").css("font-size", (fontSize * .04) + 'px');
-        $(".ar-section .goal-box .num").css("font-size", (fontSize * .027) + 'px');
+        $(".wakeel-ar .center-goal-box p").css("font-size", (fontSize * .021) + 'px');
+        $(".ar-section .goal-box .num").css("font-size", (fontSize * .025) + 'px');
     }
 
     // أول مرة لما الصفحة تتحمل
